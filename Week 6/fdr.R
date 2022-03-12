a0b9beecd12bc780d7002590593cc19638760ba2
sofiagbc <sofiagbc@gmail.com>
2022-03-12 12:06:23 -0500

    ex 2, getopt

--- Week 6/fdr.R
+++ Week 6/fdr.R
@@ -1,5 +1,21 @@
-args <- commandArgs(trailingOnly = TRUE)
+library('getopt')
+
+args <- commandArgs(TRUE)
 print(args)
+
+spec = matrix(c(
+  'verbose', 'v', 2, "integer",
+  'help'   , 'h', 0, "logical",
+  'count'  , 'c', 1, "integer",
+  'mean'   , 'm', 1, "double",
+  'sd'     , 's', 1, "double"
+), byrow=TRUE, ncol=4)
+opt = getopt(spec)
+print(opt)
+
+"""
+
+# get flags here with getopt
 
 input <- args[2]
 output <- args[4]
@@ -7,11 +23,24 @@ output <- args[4]
 input <- "p05pvalues.tsv"
 output <- "pvalues.corrected.tsv"
 
-data <- read.table(input, header=TRUE, sep= "\t")
-data$pvalue <- sort (data$pvalue, decreasing = FALSE)
+data_input <- read.table(input, header=TRUE, sep= "\t")
+m=length(data_input$index)
+
+data_input$pvalue <- as.numeric(as.character(data_input$pvalue))
+data_sorted <- order(data_input$pvalue)
+
+
+data <- data_input
+data$index <- rep(0,m)
+data$pvalue <- rep(0,m)
+data$qvalue <- rep(0,m)
+for (i in 1:m) {
+  num=data_sorted[i]
+  data$index[i]=num
+  data$pvalue[i]=data_input$pvalue[num]
+}
 
 fdr=0.05
-m=length(data$index)
 data$qvalue <- rep(0,m)
 flag <- 0
 for (i in 1:m) {
@@ -25,14 +54,13 @@ for (i in 1:m) {
 
 print(paste("Number of discoveries at fdr<=0.05 is", index))
 
-cat("index\tpvalue\tqvalue\n",file=output,append=FALSE)
+data$pvalue <- round(data$pvalue,6)
+data$qvalue <- round(data$qvalue,6)
+write.table(data,output,quote=FALSE, sep="\t",row.names=FALSE)
 
-for (i in 1:m) {
-  cat("",data$index[i],"\t",round(data$pvalue[i],6),"\t",round(data$qvalue[i],6),"\n",file=output,append=TRUE)
-}
 
 # round values from beginning or only in the output file?
 # check rounding, not correct
     
-    
+"""
 
